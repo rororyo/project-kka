f91b1d6e1015f5e13a9d0e60a01a6361170dfd47
Moch. Avin <5025221061@mhs.its.ac.id>
2023-10-24 13:04:08 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,99 @@
-import Image from 'next/image'
+'use client';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import levels from './levels';
 
 export default function Home() {
+  const [board, setBoard] = useState(levels[0].board);
+  const [activeBoard, setActiveBoard] = useState([]);
+  const [step, setStep] = useState([]);
+  const [level, setLevel] = useState(0);
+
+  useEffect(() => {
+    setBoard(levels[level].board);
+    setActiveBoard(levels[level].activeBoard);
+  }, []);
+
+  const handleClick = (indexRow, indexCol, baris) => {
+    // handle hijau
+    if(board[indexRow][indexCol] == 9) {
+      setLevel((prev) => prev + 1);
+      setBoard(levels[level + 1].board);
+      setActiveBoard(levels[level + 1].activeBoard);
+      setStep([]);
+      return;
+    }
+
+    // handle merah
+    if (board[indexRow][indexCol] == 8) return;
+
+    // handle biru
+    if (board[indexRow][indexCol] == 0) {
+      if (
+        step[step.length - 1][0] == indexRow &&
+        step[step.length - 1][1] == indexCol
+      ) {
+        // Atur board kembali putih
+        setBoard((prev) => {
+          const newBaris = [...baris];
+          newBaris[indexCol] = 1;
+          const newBoard = [...prev];
+          newBoard[indexRow] = newBaris;
+          return newBoard;
+        });
+        // Hapus step terakhir
+        setStep((prev) => prev.slice(0, -1));
+      }
+      return;
+    }
+
+    // ketika board sebelahnya aktif
+    if (
+      board[Math.max(0, indexRow - 1)][indexCol] == 0 ||
+      board[Math.min(board.length - 1, indexRow + 1)][indexCol] == 0 ||
+      board[indexRow][Math.max(0, indexCol - 1)] == 0 ||
+      board[indexRow][Math.min(board[0].length - 1, indexCol + 1)] == 0
+    ) {
+      setBoard((prev) => {
+        const newBaris = [...baris];
+        newBaris[indexCol] = 0;
+        const newBoard = [...prev];
+        newBoard[indexRow] = newBaris;
+        return newBoard;
+      });
+
+      // tambah step sekarang
+      setStep((prev) => [...prev, [indexRow, indexCol]]);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='flex flex-col items-center m-4 justify-center gap-4'>
+      <div>Level: {level + 1}</div>
+      <div>Banyak Langkah: {step.length}</div>
+      <div className='flex'>
+        <div className={`grid gap-1 grid-cols-${board[0].length}`}>
+          {board.map((baris, indexRow) => {
+            return baris.map((item, indexCol) => {
+              return (
+                <div
+                  className={`w-8 h-8 cursor-pointer
+                ${item == 1 && 'bg-white'} 
+                ${item == 8 && 'bg-red-500'} 
+                ${item == 9 && 'bg-green-500'} 
+                ${item == 0 && 'bg-blue-500'}
+                `}
+                  key={indexCol}
+                  onClick={() => handleClick(indexRow, indexCol, baris)}
+                >
+                  {/* {item == 9 && <img src='/assets/toko.png'></img>} */}
+                  {/* {item == 0 && <img src='/assets/distributor.png' className='h-full'></img>} */}
+                </div>
+              );
+            });
+          })}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
