0dff6d42352d46338f60ff272698517815efe86f
Moch. Avin <5025221061@mhs.its.ac.id>
2023-10-25 07:41:29 +0700

    feat: jump level and add levels

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,7 @@ export default function Home() {
   const [step, setStep] = useState([]);
   const [level, setLevel] = useState(0);
   const [isMouseDown, setIsMouseDown] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleDrag = (indexRow, indexCol, baris) => {
     if (isMouseDown) handleClick(indexRow, indexCol, baris);
@@ -86,15 +87,59 @@ export default function Home() {
       <ToastContainer />
       <div className='flex flex-col items-center m-4 justify-center gap-4'>
         <div className='flex flex-col text-center'>
-          <div>Level: {(level + 1) % levels.length}</div>
+          <div>Level: {(level % levels.length) + 1}</div>
           <div>Banyak Langkah: {step.length}</div>
-          <div className='flex flex-row'>
-            Mouse is Down:{' '}
-            <h4 className='font-bold text-red-400'>
-              {' '}
-              {isMouseDown == false ? 'No' : 'Yes'}
-            </h4>
+        </div>
+        <div className='relative inline-block text-left'>
+          <div>
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type='button'
+              className='inline-flex justify-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50 focus:outline-none focus:ring focus:ring-offset-2 focus:ring-blue-500 focus:ring-offset-gray-100'
+            >
+              Level: {(level % levels.length) + 1}
+              <svg
+                className='-mr-1 ml-2 h-5 w-5'
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 20 20'
+                fill='currentColor'
+                aria-hidden='true'
+              >
+                <path
+                  fillRule='evenodd'
+                  d='M14.293 7.293a1 1 0 010 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 111.414-1.414L10 9.586l2.293-2.293a1 1 0 011.414 0z'
+                />
+              </svg>
+            </button>
           </div>
+          {isOpen && (
+            <div className='origin-top-right absolute right-0 mt-2 w-full cursor-pointer rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5'>
+              <div
+                className='py-1'
+                role='menu'
+                aria-orientation='vertical'
+                aria-labelledby='options-menu'
+              >
+                {levels.map((item, index) => {
+                  return (
+                    <a
+                      onClick={() => {
+                        setLevel(index);
+                        setBoard(levels[index].board);
+                        setActiveBoard(levels[index].activeBoard);
+                        setStep([]);
+                        setIsOpen(false);
+                      }}
+                      className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'
+                      role='menuitem'
+                    >
+                      Level {index + 1}
+                    </a>
+                  );
+                })}
+              </div>
+            </div>
+          )}
         </div>
 
         <div className='flex'>
@@ -129,6 +174,13 @@ export default function Home() {
               });
             })}
           </div>
+        </div>
+        <div className='flex flex-row'>
+          Mouse is Down:{' '}
+          <h4 className='font-bold text-red-400'>
+            {' '}
+            {isMouseDown == false ? 'No' : 'Yes'}
+          </h4>
         </div>
       </div>
     </>
