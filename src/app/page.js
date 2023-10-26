19e01894bffe24c5cca6312d859b218efb5c3984
Moch. Avin <5025221061@mhs.its.ac.id>
2023-10-26 07:35:36 +0700

    fix: add key to map

--- src/app/page.js
+++ src/app/page.js
@@ -132,6 +132,7 @@ export default function Home() {
                       }}
                       className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'
                       role='menuitem'
+                      key={index}
                     >
                       Level {index + 1}
                     </a>
